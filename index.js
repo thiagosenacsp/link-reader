57a157ec86bd8e8617fa365d01c3bcae1651c4e8
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-02 22:48:59 -0300

    feat: incluir tratamento de erro com throw

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function tratarErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, "Não há arquivos neste diretório"));
+    // exibe o stacktrace no console
+}
+
 function pegarArquivo(caminhoDoArquivo) {
     const encoding = "utf-8";
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro) {
+            tratarErro(erro);
+        }
         console.log(chalk.green(texto));
     })
 }
 
-pegarArquivo("./arquivos/texto.md")
+pegarArquivo("./arquivos/")
+// para simular o erro, retire o nome do arquivo, por exemplo
