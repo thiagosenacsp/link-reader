8a4e5602e7c08f89802ef0c9badd0ee6ae035506
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-03 19:39:24 -0300

    fix: tratar caso de não existir links no arquivo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function extrairLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados;
+    return resultados.length !== 0 ? resultados : "Não há links no arquivo.";
   }
 
 function tratarErro(erro) {
