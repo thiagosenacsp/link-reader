729b47727533a01ad497f9ebe2a207b55030c191
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-03 20:11:52 -0300

    feat: trabalhar com arquivos e com diretórios

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,24 @@
 import chalk from "chalk";
+import fs from "fs";
 import pegarArquivo from "./index.js";
 
 const caminho = process.argv;
 
-async function processarTexto(caminho) {
-    const resultado = await pegarArquivo(caminho[2]);
-    console.log(chalk.yellow("Lista de links"), resultado)
+async function processarTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegarArquivo(argumentos[2]);
+        console.log(chalk.yellow("Lista de links"), resultado)
+    } else if ( fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegarArquivo(`${caminho}/${nomeDeArquivo}`)
+            console.log(`${caminho}/${nomeDeArquivo}`)
+            console.log(lista)
+        })
+        console.log(arquivos)
+    }
 }
 
-processarTexto(caminho)
+processarTexto(caminho);
