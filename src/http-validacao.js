2af9f077ba13650fd2ca8898b30d6919675a2e7d
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-03 22:27:27 -0300

    chore: extrair apenas lista de URLs

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,7 @@
+function extrairLinks (arrLinks) {
+    return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
+}
+
 export default function listaValidada (listaDeLinks) {
-    return "Entrou na função";
+    return extrairLinks(listaDeLinks);
 }
