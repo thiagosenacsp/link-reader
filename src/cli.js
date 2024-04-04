90656aa8ab352e4d05aae2c1b41b5602df24d0d3
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-03 22:21:52 -0300

    chore: criar estrutura para a função de validação

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,27 @@
 import chalk from "chalk";
 import fs from "fs";
 import pegarArquivo from "./index.js";
+import listaValidada from "./http-validacao.js";
 
 const caminho = process.argv;
 
-function imprimirLista(resultado, identificador = " ") {
+function imprimirLista(valida, resultado, identificador = " ") {
+    if (valida) {
+        console.log(
+            chalk.yellow("Lista de validada"),
+            chalk.black.bgGreen(identificador),
+            listaValidada(resultado))
+    } else {
     console.log(
         chalk.yellow("Lista de links"),
         chalk.black.bgGreen(identificador),
         resultado)
+    }
 }
 
 async function processarTexto(argumentos) {
     const caminho = argumentos[2];
+    const valida = argumentos[3] === "--validar";
 
     try {
         fs.lstatSync(caminho)
@@ -25,12 +34,12 @@ async function processarTexto(argumentos) {
 
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegarArquivo(argumentos[2]);
-        imprimirLista(resultado);
+        imprimirLista(valida, resultado);
     } else if ( fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegarArquivo(`${caminho}/${nomeDeArquivo}`)
-            imprimirLista(lista, nomeDeArquivo)
+            imprimirLista(valida, lista, nomeDeArquivo)
         })
         console.log(arquivos)
     }
